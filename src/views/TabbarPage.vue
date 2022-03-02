76f191bb1b37103a8cd6ad92376cb9c7baca9855
liaowenxi <1219585136@qq.com>
2022-03-02 15:32:05 +0800

    fix(tabbarpage.vue): 修复tabbar切换时样式失效的问题

--- src/views/TabbarPage.vue
+++ src/views/TabbarPage.vue
@@ -3,17 +3,17 @@
     <router-view/>
   </div>
   <Tabbar route v-model="active">
-    <TabbarItem icon="search" to="/home">搜索</TabbarItem>
-    <TabbarItem icon="like-o" to="collect">收藏</TabbarItem>
-    <TabbarItem icon="flower-o" to="community">社区</TabbarItem>
-    <TabbarItem icon="envelop-o" to="message">收信箱</TabbarItem>
-    <TabbarItem icon="contact" to="my">我的</TabbarItem>
+    <TabbarItem icon="search" to="/" :class="{'van-tabbar-item--active': active == 0}">搜索</TabbarItem>
+    <TabbarItem icon="like-o" to="collect" :class="{'van-tabbar-item--active': active == 1}">收藏</TabbarItem>
+    <TabbarItem icon="flower-o" to="community" :class="{'van-tabbar-item--active': active == 2}">社区</TabbarItem>
+    <TabbarItem icon="envelop-o" to="message" :class="{'van-tabbar-item--active': active == 3}">收信箱</TabbarItem>
+    <TabbarItem icon="contact" to="my" :class="{'van-tabbar-item--active': active == 4}">我的</TabbarItem>
   </Tabbar>
 </template>
 
 <script setup lang='ts'>
   import { Tabbar, TabbarItem } from 'vant';
-  import { ref } from 'vue';
+  import { ref, watch } from 'vue';
   const active = ref(0);
 </script>
 <style scoped>
